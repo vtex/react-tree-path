ef6ae5205071703750738fce75bda6c97f333b17
Guilherme Rodrigues <gadr90@gmail.com>
2016-06-24 21:41:28 +0200

    Add nested testing

--- src/test.js
+++ src/test.js
@@ -6,15 +6,36 @@ import treePath from './index.js'
 // eslint-disable-next-line
 class MyComponent extends Component {
   render () {
-    return <span id={this.props.id}>{this.props.treePath}</span>
+    return (
+      <div id={this.props.id}>
+        <span>{this.props.treePath}</span>
+        <div>{this.props.children}</div>
+      </div>
+    )
   }
 }
 
-MyComponent.propTypes = { id: PropTypes.string, treePath: PropTypes.string }
+MyComponent.propTypes = {
+  children: React.PropTypes.array,
+  id: PropTypes.string,
+  treePath: PropTypes.string,
+}
 
 const TreePathComponent = treePath()(MyComponent)
 
 test('Finds path with only one rendered element', t => {
   const wrapper = render(<TreePathComponent id="tree" />)
-  t.is(wrapper.find('#tree').text(), 'tree')
+  t.is(wrapper.find('#tree span').text(), 'tree')
+})
+
+test('Finds path with nested rendered elements', t => {
+  const wrapper = render(
+    <TreePathComponent id="outer">
+      <TreePathComponent id="inner1" />
+      <TreePathComponent id="inner2" />
+    </TreePathComponent>
+  )
+  t.is(wrapper.find('#outer > span').text(), 'outer')
+  t.is(wrapper.find('#outer #inner1 > span').text(), 'outer/inner1')
+  t.is(wrapper.find('#outer #inner2 > span').text(), 'outer/inner2')
 })
